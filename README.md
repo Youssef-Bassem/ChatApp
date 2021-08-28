29418a908bd62cfae5626443e42a3d952801d74a
SaeedGooda <saeedgooda219@gmail.com>
2021-08-28 21:39:49 +0200

    Registeration

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# chat_app
+# chat
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'auth/login/LoginScreen.dart';
+import 'auth/registeration/RegisterationScreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      routes: {
+        RegisterationScreen.routeName : (context)=>RegisterationScreen(),
+        LoginScreen.routeName : (context)=>LoginScreen(),
+      },
+      initialRoute: RegisterationScreen.routeName,
     );
   }
+
 }
